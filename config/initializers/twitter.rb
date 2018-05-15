77f051555dcf2745103cb7ca72efce6b53b87006
guilhermekbral <guilhermevannuchi@gmail.com>
2018-05-15 18:32:08 -0300

    setup: adding gems and generating files to use them

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,8 @@
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['CONSUMER_KEY']
-    config.consumer_secret     = ENV['CONSUMER_SECRET']
-    config.access_token        = ENV['ACCESS_TOKEN']
-    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-  end
+require 'twitter'
+
+CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
